5904b0a5033beb1b2d12d0807bcac02313174ac1
MarineHakobyan <marine.hakobyan4649@gmail.com>
2024-03-09 19:27:28 +0400

    fix bugs

--- update_readme.sh
+++ update_readme.sh
@@ -4,11 +4,7 @@ repo="MarineHakobyan/MarineHakobyan"
 branch="output"
 readme_file="README.md"
 
-files=$(git log --pretty=format: --name-only --diff-filter=A origin/$branch | sort - | sed '/^$/d')
-
-for file in $files
-do
   old_commit_hash=$(grep -o -m 1 '[0-9a-f]\{40\}' $readme_file)
   new_commit_hash=$(git ls-remote --heads origin $branch | awk '{print $1}')
+
   sed -i "s|$old_commit_hash|$new_commit_hash|g" $readme_file
-done
